5410afd2e4805d12fd488fed174b4ebea85f08f9
Milton Web <114828490+themiltonweb@users.noreply.github.com>
2022-11-08 11:40:13 -0800

    Add files via upload

---
+++ js/sceipt.js
@@ -0,0 +1,50 @@
+$(function(){
+
+    // Hide
+    $("#show_hide1").click(function(){
+        $(".One").toggle(1200);
+    })
+    
+
+    // Hide
+    $("#show_hide2").click(function(){
+        $(".Two").toggle(1200);
+    })
+
+    // Hide
+    $("#show_hide3").click(function(){
+        $(".Three").toggle(1200);
+    })
+
+    // Hide
+    $("#show_hide4").click(function(){
+        $(".Fore").toggle(1200);
+    })
+
+    // Hide
+    $("#show_hide5").click(function(){
+        $(".Five").toggle(1200);
+    })
+
+    // Hide
+    $("#show_hide7").click(function(){
+        $(".Sevine").toggle(1200);
+    })
+
+    // Hide
+    $("#show_hide8").click(function(){
+        $(".Eight").toggle(1200);
+    })
+
+    // Hide
+    $("#show_hide9").click(function(){
+        $(".Nine").toggle(1200);
+    })
+
+    // Hide
+    $("#show_hide10").click(function(){
+        $(".Ten").toggle(1200);
+    })
+
+
+});
